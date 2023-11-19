2505852f7cc4eda882631d68f67103fb94ba1d38
Chi-Han Huang <huangchihan@gmail.com>
2023-11-19 11:40:26 +0800

    initial commit

---
+++ lua/plugins/vim-go.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "fatih/vim-go",
+    lazy = false,
+    cmd = "GoUpdateBinaries",
+    config = function()
+      vim.g.go_gopls_enabled = 1
+      vim.g.go_fmt_command = "goimports"
+      vim.g.go_def_mapping_enabled = 1
+      vim.g.syntastic_mode_map = { mode = "active", passive_filetypes = { "go" } }
+
+      -- Auto-format on save
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("GoAutoFmt", { clear = true }),
+        pattern = "*.go",
+        command = "silent! GoFmt",
+      })
+    end,
+  },
+}
