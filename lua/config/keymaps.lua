a8d208c196a702fde43b5f9a36099df45b15fddb
Chi-Han Huang <huangchihan@gmail.com>
2023-11-20 10:38:31 +0800

    Added tpope surround

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,22 +4,3 @@
 
 vim.api.nvim_set_keymap("n", "P", ":bprevious<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "N", ":bnext<CR>", { noremap = true, silent = true })
-
--- lsp-zero bindings
---local lsp_zero = require("lsp-zero")
---
---lsp_zero.on_attach(function(client, bufnr)
---  -- see :help lsp-zero-keybindings
---  -- to learn the available actions
---  lsp_zero.default_keymaps({ buffer = bufnr })
---end)
---
---local cmp = require("cmp")
---cmp.setup({
---  mapping = cmp.mapping.preset.insert({
---    ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
---    ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
---  }),
---})
---
---require("lspconfig").gopls.setup({})
