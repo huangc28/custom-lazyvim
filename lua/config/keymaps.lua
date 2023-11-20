3c8c85d67cfce8db18b15771d56930c0a70f6715
Chi-Han Huang <huangchihan@gmail.com>
2023-11-20 10:30:11 +0800

    Refactor key bindings of lsp.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,21 +6,20 @@ vim.api.nvim_set_keymap("n", "P", ":bprevious<CR>", { noremap = true, silent = t
 vim.api.nvim_set_keymap("n", "N", ":bnext<CR>", { noremap = true, silent = true })
 
 -- lsp-zero bindings
-local lsp_zero = require("lsp-zero")
-
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({ buffer = bufnr })
-end)
-
-local cmp = require("cmp")
--- local cmp_action = lsp_zero.cmp_action()
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-  }),
-})
-
-require("lspconfig").gopls.setup({})
+--local lsp_zero = require("lsp-zero")
+--
+--lsp_zero.on_attach(function(client, bufnr)
+--  -- see :help lsp-zero-keybindings
+--  -- to learn the available actions
+--  lsp_zero.default_keymaps({ buffer = bufnr })
+--end)
+--
+--local cmp = require("cmp")
+--cmp.setup({
+--  mapping = cmp.mapping.preset.insert({
+--    ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+--    ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+--  }),
+--})
+--
+--require("lspconfig").gopls.setup({})
--- lua/plugins/zero-lsp.lua
+++ lua/plugins/zero-lsp.lua
@@ -1,7 +1,22 @@
 return {
-  { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-  { "neovim/nvim-lspconfig" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/nvim-cmp" },
-  { "L3MON4D3/LuaSnip" },
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    opts = function()
+      local cmp = require("cmp")
+
+      return {
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        }),
+      }
+    end,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+    },
+  },
 }
