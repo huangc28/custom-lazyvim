830e575412e5a247eba3b09ea1f79beb9c03e0f8
Chi-Han Huang <huangchihan@gmail.com>
2023-11-20 11:08:53 +0800

    Added autocmd to indent go file correctly.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+if vim.fn.has("autocmd") == 1 then
+  local styles_group = vim.api.nvim_create_augroup("indent", { clear = true })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = styles_group,
+    pattern = "go",
+    callback = function()
+      vim.opt_local.expandtab = false
+      vim.opt_local.tabstop = 8
+      vim.opt_local.shiftwidth = 8
+      vim.opt_local.softtabstop = 8
+    end,
+  })
+end
