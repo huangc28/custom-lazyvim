c13c692151317cae516f2e74f66e686c2b071d2b
Chi-Han Huang <huangchihan@gmail.com>
2023-11-24 16:27:50 +0800

    Fixed lsp problem.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,21 +1,13 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  opts = function()
+  config = function()
     local cmp = require("cmp")
 
-    return {
-      mappings = {
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
         ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-      },
-    }
+      }),
+    })
   end,
-
-  dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",
-    "rafamadriz/friendly-snippets",
-  },
 }
