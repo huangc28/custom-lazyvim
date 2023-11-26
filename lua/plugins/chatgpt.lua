b4955c4c7059f9dba3c3167a1f9b02de66cc9846
Chi-Han Huang <huangchihan@gmail.com>
2023-11-26 22:52:15 +0800

    ChatGPT nvim tinkering

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,23 @@
+-- config reference: https://github.com/jackMort/ChatGPT.nvim/blob/9508429d514509b96d8ca9ec3f16933cfb30ac8a/lua/chatgpt/config.lua#L10-L157
+return {
+  "jackMort/ChatGPT.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  lazy = false,
+  config = function()
+    local chatgpt = require("chatgpt")
+    chatgpt.setup({
+      api_key_cmd = "pass show bryan_keystore/chatgpt-nvim",
+
+      openai_params = {
+        model = "gpt-4",
+      },
+      openai_edit_params = {
+        model = "gpt-4",
+      },
+    })
+  end,
+}
