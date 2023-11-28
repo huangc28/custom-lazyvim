95c452ad03be32f3bd77ef3488eb33c289442a19
Chi-Han Huang <huangchihan@gmail.com>
2023-11-28 13:59:32 +0800

    Restore cursor position correctly

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -22,7 +22,7 @@ if vim.fn.has("autocmd") == 1 then
     callback = function()
       local save_cursor = vim.api.nvim_win_get_cursor(0)
       vim.cmd([[ %s/\s\+$//e ]])
-      vim.api.nvim_win_get_cursor(0, save_cursor)
+      vim.api.nvim_win_set_cursor(0, save_cursor)
     end,
   })
 end
